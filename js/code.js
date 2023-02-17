b2c789f85e8a4f19232d7dd1a0e675fd1b8167b7
michal gozdek <michalgozdek12@gmail.com>
2023-02-17 14:54:15 +0100

    changetoconstinjs

--- js/code.js
+++ js/code.js
@@ -1,19 +1,19 @@
-let PLNcurency = document.querySelector(".js-PLN");
-let curencyElemnt = document.querySelector(".js-curency");
-let formElement = document.querySelector(".js-form");
-let resultCurency = document.querySelector(".js-result");
-let resetElement = document.querySelector(".js-reset")
+const PLNcurency = document.querySelector(".js-PLN");
+const curencyElemnt = document.querySelector(".js-curency");
+const formElement = document.querySelector(".js-form");
+const resultCurency = document.querySelector(".js-result");
+const resetElement = document.querySelector(".js-reset")
 
-let USD = 4.41;
-let EUR = 4.74;
+const USD = 4.41;
+const EUR = 4.74;
 
 formElement.addEventListener("submit", (Event) => {
     Event.preventDefault();
 
 
 
-    let curency = curencyElemnt.value
-    let PLN = +PLNcurency.value;
+    const curency = curencyElemnt.value
+    const PLN = +PLNcurency.value;
     let result;
 
     switch (curency) {
