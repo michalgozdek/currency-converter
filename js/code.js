d165018a835238345a9bf47cf5e379585c432cb0
michal gozdek <michalgozdek12@gmail.com>
2023-02-17 17:59:37 +0100

    addNewFucctioninJS

--- js/code.js
+++ js/code.js
@@ -1,34 +1,48 @@
-const PLNcurency = document.querySelector(".js-PLN");
-const curencyElemnt = document.querySelector(".js-curency");
-const formElement = document.querySelector(".js-form");
-const resultCurency = document.querySelector(".js-result");
-const resetElement = document.querySelector(".js-reset")
+{
+    const PLNcurency = document.querySelector(".js-PLN");
+    const curencyElemnt = document.querySelector(".js-curency");
+    const formElement = document.querySelector(".js-form");
+    const resultCurency = document.querySelector(".js-result");
+    const resetElement = document.querySelector(".js-reset")
 
-const USD = 4.41;
-const EUR = 4.74;
-
-formElement.addEventListener("submit", (Event) => {
-    Event.preventDefault();
+   
 
 
+    const curencyResult = (PLN, curency) => 
+    {
+        const USD = 4.41;
+        const EUR = 4.74;
+    
 
-    const curency = curencyElemnt.value
-    const PLN = +PLNcurency.value;
-    let result;
+       
+       
 
-    switch (curency) {
-        case "USD":
-            result = PLN / USD;
-            break;
-        case "EUR":
-            result = PLN / EUR;
+        switch (curency) {
+            case "USD":
+                return PLN / USD;
+                
+            case "EUR":
+                return PLN / EUR;
 
-            break;
-    }
-
-
+            
+        }
+    };
 
 
-    resultCurency.innerHTML = `${"Kwota po przeliczniu"} ${PLN.toFixed(2)}  PLN= <strong>${result.toFixed(2)} ${curency}</strong `;
-});
+    formElement.addEventListener("submit", (Event) => {
+        Event.preventDefault();
 
+
+
+
+        const curency = curencyElemnt.value
+        const PLN = +PLNcurency.value;
+
+
+            const result = curencyResult(PLN ,curency);
+
+        resultCurency.innerHTML = `${"Kwota po przeliczniu"} ${PLN.toFixed(2)}  PLN= <strong>${result.toFixed(2)} ${curency}</strong> `;
+    });
+
+
+}
